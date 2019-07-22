76736498170f19145976c1b1c83f427ff8953771
Meng YuCheng <maxmeng93@gmail.com>
2019-07-22 13:23:20 +0800

    增加兼职平台

--- assets/js/data.js
+++ assets/js/data.js
@@ -442,9 +442,34 @@ var data = [
     ]
   },
   {
-    title: '工作',
+    title: '兼职平台',
     icon: 'linecons-user',
-    list: []
+    list: [
+      {
+        title: '程序员客栈',
+        image: './assets/images/logo/proginn.webp',
+        href: 'https://www.proginn.com/',
+        desc: '领先的程序员自由工作平台'
+      },
+      {
+        title: '开源众包',
+        image: './assets/images/logo/zb.oschina.ico',
+        href: 'https://zb.oschina.net/',
+        desc: '百万开发者，为您提供专业的软件开发服务'
+      },
+      {
+        title: '码市',
+        image: './assets/images/logo/codemart.png',
+        href: 'https://codemart.com/',
+        desc: '互联网软件外包服务平台'
+      },
+      {
+        title: '英选',
+        image: './assets/images/logo/yingxuan.png',
+        href: 'https://www.yingxuan.io/',
+        desc: '可信赖的定制开发外包服务'
+      },
+    ]
   },
   {
     title: '视频资源',
