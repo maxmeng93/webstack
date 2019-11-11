720d600a501ca682da2e61e463fb5b8527b78811
maxmeng <maxmeng93@gmail.com>
2019-11-11 14:44:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-// import 'react-app-polyfill/ie9';
-// import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'antd/dist/antd.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
